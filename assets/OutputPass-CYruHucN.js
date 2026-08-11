ec048d9cb9db745d3411db138b1b978dad2d2677
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-11 09:09:40 +0000

    V5 web build: + GLB pistol/shotgun, weapons iter3-5 0909

--- assets/OutputPass-CYruHucN.js
+++ assets/OutputPass-CYruHucN.js
@@ -1,4 +1,4 @@
-import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{l as r,u as i}from"./index-CurEIg_z.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{l as r,u as i}from"./index-D1F36FBi.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
--- assets/UnrealBloomPass-p3E6Be-1.js
+++ assets/UnrealBloomPass-p3E6Be-1.js
@@ -1,4 +1,4 @@
-import{P as e,Yt as t,fn as n,ln as r,on as i,p as a,st as o,un as s}from"./three-BOMTFBZf.js";import{d as c,l,u}from"./index-CurEIg_z.js";var d={name:`LuminosityHighPassShader`,shaderID:`luminosityHighPass`,uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new a(0)},defaultOpacity:{value:0}},vertexShader:`
+import{P as e,Yt as t,fn as n,ln as r,on as i,p as a,st as o,un as s}from"./three-BOMTFBZf.js";import{d as c,l,u}from"./index-D1F36FBi.js";var d={name:`LuminosityHighPassShader`,shaderID:`luminosityHighPass`,uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new a(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
