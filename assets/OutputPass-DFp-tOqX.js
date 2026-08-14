c9da62d1941a1ea82496533d1242cba93ae5fff0
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 18:37:46 +0000

    deploy iter-54

--- assets/OutputPass-DFp-tOqX.js
+++ assets/OutputPass-DFp-tOqX.js
@@ -1,4 +1,4 @@
-import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-BCNe_HP4.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-CqRfZ_MI.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
