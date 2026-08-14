a7fd3e9d6e10f535e3aefeed016c9084ae3230d2
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 13:53:56 +0000

    deploy iter-31

--- assets/OutputPass-Rw45GrLK.js
+++ assets/OutputPass-Rw45GrLK.js
@@ -1,4 +1,4 @@
-import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{f as r,p as i}from"./index-htZm2ym3.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{f as r,p as i}from"./index-CuT5UAXp.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
