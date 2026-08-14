92e2868607ff49bda706fef56f5c1c5a1064a77d
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 13:41:51 +0000

    deploy iter-29 potato fast path

--- assets/UnrealBloomPass-C1d6qkwf.js
+++ assets/UnrealBloomPass-C1d6qkwf.js
@@ -1,4 +1,4 @@
-import{P as e,Yt as t,fn as n,ln as r,on as i,p as a,st as o,un as s}from"./three-BOMTFBZf.js";import{f as c,m as l,p as u}from"./index-BSWNtf1n.js";var d={name:`LuminosityHighPassShader`,shaderID:`luminosityHighPass`,uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new a(0)},defaultOpacity:{value:0}},vertexShader:`
+import{P as e,Yt as t,fn as n,ln as r,on as i,p as a,st as o,un as s}from"./three-BOMTFBZf.js";import{f as c,m as l,p as u}from"./index-htZm2ym3.js";var d={name:`LuminosityHighPassShader`,shaderID:`luminosityHighPass`,uniforms:{tDiffuse:{value:null},luminosityThreshold:{value:1},smoothWidth:{value:1},defaultColor:{value:new a(0)},defaultOpacity:{value:0}},vertexShader:`
 
 		varying vec2 vUv;
 
