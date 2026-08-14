3afb1f309b27ba61b4aa1d8dde7f047c1c6d5831
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 14:55:32 +0000

    deploy iter-36

--- assets/OutputPass-DTDHolFD.js
+++ assets/OutputPass-DTDHolFD.js
@@ -1,4 +1,4 @@
-import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{f as r,p as i}from"./index-CPE8UKot.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{f as r,p as i}from"./index-DG0pkdry.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
