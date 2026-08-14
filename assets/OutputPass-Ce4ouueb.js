84dee904a42c299eeae2138121a80068af9ec5fa
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 18:08:07 +0000

    deploy iter-51: program-leak fix + draco models

--- assets/OutputPass-Ce4ouueb.js
+++ assets/OutputPass-Ce4ouueb.js
@@ -1,4 +1,4 @@
-import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-Doa4SKHV.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-NWf67Ojn.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
