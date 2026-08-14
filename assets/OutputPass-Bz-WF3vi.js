1a565f8d88af711e8b5470f0d3a96fd2cacedd1d
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 22:25:06 +0000

    deploy iter-69

--- assets/OutputPass-Bz-WF3vi.js
+++ assets/OutputPass-Bz-WF3vi.js
@@ -1,4 +1,4 @@
-import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-Bqu9Tawx.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-CmLSfgdW.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
