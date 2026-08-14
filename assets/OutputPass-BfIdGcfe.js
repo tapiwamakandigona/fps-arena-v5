4f1bf8008a51967755f7fe2552dbb12bcef0224e
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 11:45:04 +0000

    deploy iter23: boss rigs

--- assets/OutputPass-BfIdGcfe.js
+++ assets/OutputPass-BfIdGcfe.js
@@ -1,4 +1,4 @@
-import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{d as r,u as i}from"./index-C4TZvqyo.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{d as r,u as i}from"./index-kmmCH959.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
