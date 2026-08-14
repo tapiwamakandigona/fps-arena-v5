fba5b5fe48c69aad791d50a24f8ed05de93fe23d
Tapiwa Makandigona <tapiwamakandigoner@gmail.com>
2026-08-14 15:56:19 +0000

    deploy iter-39: matcap viewmodel R0-R1

--- assets/OutputPass-Banm9eOq.js
+++ assets/OutputPass-Banm9eOq.js
@@ -1,4 +1,4 @@
-import{Bt as e,m as t,on as n}from"./three-BOMTFBZf.js";import{f as r,p as i}from"./index-DIqT1NO3.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
+import{Vt as e,m as t,sn as n}from"./three-BrqJoJeB.js";import{f as r,p as i}from"./index-CehYnX1k.js";var a={name:`OutputShader`,uniforms:{tDiffuse:{value:null},toneMappingExposure:{value:1}},vertexShader:`
 		precision highp float;
 
 		uniform mat4 modelViewMatrix;
